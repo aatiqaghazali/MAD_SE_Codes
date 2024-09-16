230d598183346a481a42dfb2dcaacf66deec6bcb
aatiqa <aatiqaghazali@gmail.com>
2024-09-16 15:34:18 +0500

    code added for week5

---
+++ basics.js
@@ -0,0 +1,5 @@
+let arr=function ()
+{
+ console.log('aatiqa');
+} 
+arr();
---
+++ var_scope.js
@@ -0,0 +1,20 @@
+var first_name="aatiqa";
+function get_name()
+{
+
+    var last_name="ghazali";
+    console.log(first_name);
+    console.log(last_name);
+    if(2<3)
+    {
+        let middle_name='bint e';
+        const another_name='ayesha';
+        console.log(middle_name);
+        middle_name='bin';
+        another_name='khola'
+    }
+    //console.log(middle_name)
+}
+get_name();
+console.log(first_name);
+//console.log(last_name);
